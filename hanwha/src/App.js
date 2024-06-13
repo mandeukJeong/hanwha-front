8167363b984e964697cd5176183676dc77aadf0f
mandeukJeong <wjdalstj137@gmail.com>
2024-06-13 19:22:52 +0900

    style: favicon 설정

--- hanwha/src/App.js
+++ hanwha/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 const App = () => {
-  return <div>main</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 };
 
 export default App;
