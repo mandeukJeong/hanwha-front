9a3b40976ced7933c3a9c385bb1504e88faab726
mandeukJeong <wjdalstj137@gmail.com>
2024-06-21 18:02:40 +0900

    style: Player detail season desktop

--- hanwha/src/pages/players/PlayerDetail.js
+++ hanwha/src/pages/players/PlayerDetail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import { COLORS } from '../../constants/colors';
 import Ryu from '../../assets/Ryu.png';
 
@@ -16,8 +17,8 @@ const DetailWrap = styled.div`
 const PlayerWrap = styled.div`
   width: 100%;
   display: flex;
-  padding: 50px 0;
-  gap: 250px;
+  padding: 50px 0 200px 0;
+  gap: 200px;
   justify-content: center;
   img {
     width: 35%;
@@ -87,6 +88,82 @@ const ProfileWrap = styled.div`
     font-size: 16px;
   }
 `;
+const SeasonWrap = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 50px;
+  h1 {
+    color: ${COLORS.white};
+    font-size: 25px;
+    font-weight: 700;
+    margin-bottom: 50px;
+  }
+`;
+const ScoreWrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 50px;
+`;
+const ScoreGraph = styled.div`
+  background-color: ${COLORS.dark};
+  width: 30%;
+  height: 300px;
+  border-radius: 20px;
+`;
+const ScoreRate = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 50px 0;
+  gap: 50px;
+  background-color: ${COLORS.dark};
+  width: 40%;
+  height: 300px;
+  border-radius: 20px;
+`;
+const ScoreDetail = styled.div`
+  width: 25%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: center;
+  justify-content: center;
+  h3 {
+    color: ${COLORS.orange};
+    font-weight: 800;
+    font-size: 30px;
+  }
+  h4 {
+    color: ${COLORS.grey};
+    font-weight: 700;
+    font-size: 15px;
+  }
+`;
+const CommonLink = styled(Link)`
+  border: 2px solid ${COLORS.grey};
+  border-radius: 5px;
+  padding: 20px 50px;
+  text-decoration: none;
+  color: ${COLORS.white};
+  font-size: 15px;
+  font-weight: 300;
+  background: linear-gradient(
+    to right,
+    ${COLORS.orange} 50%,
+    rgba(0, 0, 0, 0) 50%
+  );
+  background-size: 200% 100%;
+  background-position: right bottom;
+  transition: all 0.5s ease-out;
+
+  &:hover {
+    border: 2px solid ${COLORS.orange};
+    background-position: left bottom;
+  }
+`;
 
 const PlayerDetail = () => {
   return (
@@ -126,6 +203,39 @@ const PlayerDetail = () => {
           </ProfileWrap>
         </InfoWrap>
       </PlayerWrap>
+      <SeasonWrap>
+        <h1>2024 SEASON</h1>
+        <ScoreWrap>
+          <ScoreGraph></ScoreGraph>
+          <ScoreRate>
+            <ScoreDetail>
+              <h3>5.91</h3>
+              <h4>평균자책점</h4>
+            </ScoreDetail>
+            <ScoreDetail>
+              <h3>1</h3>
+              <h4>승</h4>
+            </ScoreDetail>
+            <ScoreDetail>
+              <h3>3</h3>
+              <h4>패</h4>
+            </ScoreDetail>
+            <ScoreDetail>
+              <h3>31</h3>
+              <h4>탈삼진</h4>
+            </ScoreDetail>
+            <ScoreDetail>
+              <h3>32</h3>
+              <h4>이닝</h4>
+            </ScoreDetail>
+            <ScoreDetail>
+              <h3>0</h3>
+              <h4>세이브</h4>
+            </ScoreDetail>
+          </ScoreRate>
+        </ScoreWrap>
+      </SeasonWrap>
+      <CommonLink to="/player/pitcher">GO TO LIST</CommonLink>
     </DetailWrap>
   );
 };
