245f4054d25e4dcad3a09072840a5aaa32d31276
mandeukJeong <wjdalstj137@gmail.com>
2024-07-01 19:06:04 +0900

    style: players(detail) mobile

--- hanwha/src/pages/players/PlayerDetail.js
+++ hanwha/src/pages/players/PlayerDetail.js
@@ -1,242 +1,325 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 import { COLORS } from '../../constants/colors';
+import { SIZES } from '../../constants/size';
+import { mediaMax, mediaMin } from '../../utils/media';
+import CustomBtn from '../../components/common/CustomBtn';
 import Ryu from '../../assets/players/Ryu.png';
 
-const DetailWrap = styled.div`
-  width: 100%;
-  padding-top: 75.05px;
+const MainWrap = styled.main`
+  color: ${COLORS.white};
   background-color: ${COLORS.black};
-  padding: 75.05px 20px 50px 20px;
+`;
+const ProfileSection = styled.section`
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
+  gap: 15em;
+  padding-top: 50px;
+  ${mediaMax.medium`
+    padding-top: 20px;
+    flex-direction: column;
+    gap: 2em;
+  `};
+  ${mediaMax.small`
+    padding-top: 10px;
+    gap: 1.5em;
+  `};
 `;
-const PlayerWrap = styled.div`
-  width: 100%;
-  display: flex;
-  padding: 50px 0 200px 0;
-  gap: 200px;
-  justify-content: center;
-  img {
-    width: 35%;
-    max-width: 550px;
-  }
+const ProfileImg = styled.img`
+  width: 30%;
+  ${mediaMax.medium`
+    width: 50%;
+  `};
+  ${mediaMax.small`
+    width: 70%;
+  `};
+`;
+const ProfileWrap = styled.div`
+  width: 30%;
+  ${mediaMax.medium`
+    width: 50%;
+  `};
+  ${mediaMax.small`
+    width: 70%;
+  `};
 `;
 const InfoWrap = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 30px;
+  align-items: center;
+  gap: 20px;
+  margin-bottom: 1em;
+  ${mediaMax.medium`
+    justify-content: center;
+    margin-bottom: 2em;
+  `};
+  ${mediaMax.small`
+    gap: 15px;
+    margin-bottom: 1.5em;
+  `};
+  h2 {
+    color: ${COLORS.grey};
+    font-weight: 700;
+    vertical-align: center;
+    font-size: 75px;
+    height: 75px;
+    line-height: 75px;
+    margin-bottom: 20px;
+    ${mediaMax.small`
+      font-size: 50px;
+      height: 50px;
+      line-height: 50px;
+      margin-bottom: 6px;
+    `};
+  }
 `;
 const NameWrap = styled.div`
-  display: flex;
-  color: ${COLORS.grey};
-  gap: 10px;
-  div {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: 10px;
-  }
-  h2 {
-    font-size: 80px;
-    font-weight: 800;
-  }
+  height: 75px;
+  ${mediaMax.small`
+    height: 50px;
+  `};
   h3 {
-    font-size: 25px;
-    color: ${COLORS.white};
+    font-weight: 400;
+    font-size: ${SIZES.ltlarge};
+    ${mediaMax.small`
+      font-size: ${SIZES.tblarge};
+    `};
   }
-  h4 {
-    font-size: 20px;
+  p {
+    color: ${COLORS.grey};
+    font-weight: 500;
+    font-size: ${SIZES.ltmedium};
+    ${mediaMax.small`
+      font-size: ${SIZES.tbmedium};
+    `};
   }
 `;
 const PositionWrap = styled.div`
-  color: ${COLORS.white};
   display: flex;
   align-items: center;
-  gap: 10px;
-  h3 {
-    font-size: 22px;
-    font-weight: 600;
-    margin-bottom: 10px;
-  }
-  h4 {
-    font-size: 16px;
-    font-weight: 400;
-  }
+  gap: 20px;
+  margin-bottom: 2em;
+  ${mediaMax.medium`
+    margin-bottom: 3em;
+  `};
+  ${mediaMax.small`
+    gap: 15px;
+    margin-bottom: 2em;
+  `};
 `;
 const PositionImg = styled.div`
-  width: 95px;
-  height: 95px;
-  border-radius: 20px;
   background-color: ${COLORS.dark};
+  border-radius: 10px;
+  width: 80px;
+  height: 80px;
+  ${mediaMax.small`
+    width: 70px;
+    height: 70px;
+  `};
 `;
-const ProfileWrap = styled.div`
-  display: flex;
-  gap: 15px;
-  h4 {
-    color: ${COLORS.grey};
-    font-size: 16px;
+const PositionText = styled.div`
+  h3 {
     font-weight: 600;
-    width: 60px;
+    font-size: ${SIZES.ltmedium};
+    margin-bottom: 3px;
+    ${mediaMax.small`
+      font-size: ${SIZES.tbmedium};
+      margin-bottom: 5px;
+    `};
   }
   p {
-    color: ${COLORS.white};
-    font-size: 16px;
+    font-size: ${SIZES.ltsmall};
+    ${mediaMax.small`
+      font-size: ${SIZES.tbsmall};
+    `};
+  }
+`;
+const PersonalDetail = styled.div`
+  display: flex;
+  margin-bottom: 1.5em;
+  h3 {
+    color: ${COLORS.grey};
+    font-weight: 600;
+    font-size: ${SIZES.ltsmall};
+    width: 70px;
+    ${mediaMax.small`
+      font-size: ${SIZES.tbsmall};
+      width: 65px;
+    `};
+  }
+  p {
+    font-size: ${SIZES.ltsmall};
+    ${mediaMax.small`
+      font-size: ${SIZES.tbsmall};
+    `};
+  }
+`;
+const SeasonSection = styled.section`
+  text-align: center;
+  padding: 100px 0;
+  ${mediaMax.medium`
+    padding: 80px 0;
+  `};
+  ${mediaMax.small`
+    padding: 50px 0;
+  `};
+  h1 {
+    font-weight: 700;
+    font-size: 30px;
+    margin-bottom: 60px;
+    ${mediaMax.medium`
+      margin-bottom: 50px;
+    `};
+    ${mediaMax.small`
+      font-size: 25px;
+      margin-bottom: 30px;
+    `};
   }
 `;
 const SeasonWrap = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 50px;
-  h1 {
-    color: ${COLORS.white};
-    font-size: 25px;
-    font-weight: 700;
-    margin-bottom: 50px;
-  }
-`;
-const ScoreWrap = styled.div`
-  width: 100%;
   display: flex;
   justify-content: center;
-  gap: 50px;
+  margin-bottom: 60px;
+  ${mediaMin.large`
+    gap: 5em;
+  `};
+  ${mediaMax.medium`
+    margin-bottom: 50px;
+  `};
+  ${mediaMax.small`
+    margin-bottom: 30px;
+  `};
 `;
-const ScoreGraph = styled.div`
+const ScoreImg = styled.div`
   background-color: ${COLORS.dark};
-  width: 30%;
-  height: 300px;
   border-radius: 20px;
+  width: 30%;
+  height: 350px;
+  ${mediaMax.medium`
+    display: none;
+  `};
 `;
-const ScoreRate = styled.div`
+const ScoreWrap = styled.div`
+  background-color: ${COLORS.dark};
+  border-radius: 20px;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-  padding: 50px 0;
-  gap: 50px;
-  background-color: ${COLORS.dark};
+  align-items: center;
   width: 40%;
-  height: 300px;
-  border-radius: 20px;
+  height: 350px;
+  gap: 0.5em;
+  ${mediaMax.medium`
+    width: 70%;
+    height: 300px;
+  `};
+  ${mediaMax.small`
+    width: 80%;
+    height: 250px;
+    gap: 0.3em;
+  `};
 `;
 const ScoreDetail = styled.div`
-  width: 25%;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  align-items: center;
-  justify-content: center;
+  width: 30%;
   h3 {
     color: ${COLORS.orange};
-    font-weight: 800;
-    font-size: 30px;
-  }
-  h4 {
-    color: ${COLORS.grey};
     font-weight: 700;
-    font-size: 15px;
+    margin-bottom: 0.1em;
+    font-size: ${SIZES.ltxlarge};
+    ${mediaMax.small`
+      font-size: ${SIZES.tbxlarge};
+    `};
   }
-`;
-const CommonLink = styled(Link)`
-  border: 2px solid ${COLORS.grey};
-  border-radius: 5px;
-  padding: 20px 50px;
-  text-decoration: none;
-  color: ${COLORS.white};
-  font-size: 15px;
-  font-weight: 300;
-  background: linear-gradient(
-    to right,
-    ${COLORS.orange} 50%,
-    rgba(0, 0, 0, 0) 50%
-  );
-  background-size: 200% 100%;
-  background-position: right bottom;
-  transition: all 0.5s ease-out;
-
-  &:hover {
-    border: 2px solid ${COLORS.orange};
-    background-position: left bottom;
+  p {
+    color: ${COLORS.grey};
+    font-weight: 600;
+    font-size: ${SIZES.ltsmall};
+    ${mediaMax.small`
+      font-size: ${SIZES.tbsmall};
+    `};
   }
 `;
 
 const PlayerDetail = () => {
   return (
-    <DetailWrap>
-      <PlayerWrap>
-        <img src={Ryu} alt="류현진" />
-        <InfoWrap>
-          <NameWrap>
+    <MainWrap>
+      <ProfileSection>
+        <ProfileImg src={Ryu} alt="류현진" />
+        <ProfileWrap>
+          <InfoWrap>
             <h2>99</h2>
-            <div>
+            <NameWrap>
               <h3>류현진</h3>
-              <h4>RYU HYUN JIN</h4>
-            </div>
-          </NameWrap>
+              <p>RYU HYUN JIN</p>
+            </NameWrap>
+          </InfoWrap>
           <PositionWrap>
-            <PositionImg />
-            <div>
+            <PositionImg></PositionImg>
+            <PositionText>
               <h3>투수</h3>
-              <h4>좌/우</h4>
-            </div>
+              <p>좌/우</p>
+            </PositionText>
           </PositionWrap>
-          <ProfileWrap>
-            <h4>생년월일</h4>
-            <p>1987.03.25</p>
-          </ProfileWrap>
-          <ProfileWrap>
-            <h4>체격</h4>
-            <p>187cm, 98kg</p>
-          </ProfileWrap>
-          <ProfileWrap>
-            <h4>출신학교</h4>
-            <p>인천 창영초-동산중-동산고-대전대</p>
-          </ProfileWrap>
-          <ProfileWrap>
-            <h4>경력</h4>
-            <p>한화-LA다져스-토론토</p>
-          </ProfileWrap>
-        </InfoWrap>
-      </PlayerWrap>
-      <SeasonWrap>
+          <div>
+            <PersonalDetail>
+              <h3>생년월일</h3>
+              <p>1987.03.25</p>
+            </PersonalDetail>
+            <PersonalDetail>
+              <h3>체격</h3>
+              <p>187cm, 98kg</p>
+            </PersonalDetail>
+            <PersonalDetail>
+              <h3>출신학교</h3>
+              <p>인천 창영초-동산중-동산고-대전대</p>
+            </PersonalDetail>
+            <PersonalDetail>
+              <h3>경력</h3>
+              <p>한화-LA다져스-토론토</p>
+            </PersonalDetail>
+          </div>
+        </ProfileWrap>
+      </ProfileSection>
+      <SeasonSection>
         <h1>2024 SEASON</h1>
-        <ScoreWrap>
-          <ScoreGraph></ScoreGraph>
-          <ScoreRate>
+        <SeasonWrap>
+          <ScoreImg></ScoreImg>
+          <ScoreWrap>
             <ScoreDetail>
               <h3>5.91</h3>
-              <h4>평균자책점</h4>
+              <p>평균자책점</p>
             </ScoreDetail>
             <ScoreDetail>
               <h3>1</h3>
-              <h4>승</h4>
+              <p>승</p>
             </ScoreDetail>
             <ScoreDetail>
               <h3>3</h3>
-              <h4>패</h4>
+              <p>패</p>
             </ScoreDetail>
             <ScoreDetail>
               <h3>31</h3>
-              <h4>탈삼진</h4>
+              <p>탈삼진</p>
             </ScoreDetail>
             <ScoreDetail>
               <h3>32</h3>
-              <h4>이닝</h4>
+              <p>이닝</p>
             </ScoreDetail>
             <ScoreDetail>
               <h3>0</h3>
-              <h4>세이브</h4>
+              <p>세이브</p>
             </ScoreDetail>
-          </ScoreRate>
-        </ScoreWrap>
-      </SeasonWrap>
-      <CommonLink to="/player/pitcher">GO TO LIST</CommonLink>
-    </DetailWrap>
+          </ScoreWrap>
+        </SeasonWrap>
+        <CustomBtn
+          to="/players/pitcher"
+          $border={COLORS.grey}
+          $fontColor={COLORS.white}
+          $bgColor={COLORS.orange}
+          text="GO TO LIST"
+        />
+      </SeasonSection>
+    </MainWrap>
   );
 };
 
