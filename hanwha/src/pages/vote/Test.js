997666e293474fac954fe8004b26a557e6409e14
mandeukJeong <wjdalstj137@gmail.com>
2024-07-02 14:53:50 +0900

    style: vote(end) mobile

--- hanwha/src/pages/vote/Test.js
+++ hanwha/src/pages/vote/Test.js
@@ -1,156 +1,75 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { COLORS } from '../../constants/colors';
-import Hwang from '../../assets/Hwang.png';
-import Ryu from '../../assets/players/Ryu.png';
-import Moon from '../../assets/players/Moon.png';
-import Lee from '../../assets/players/Lee.png';
+import vote_end from '../../assets/vote_end.png';
 
+const fadeIn = keyframes`
+    from {
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
+`;
 const VoteWrap = styled.div`
   width: 100%;
-  padding: 125.05px 50px 100px 50px;
-  display: flex;
-  flex-direction: column;
+  height: 100vh;
+  padding: 175.05px 0 100px 0;
   background-color: ${COLORS.black};
   color: ${COLORS.white};
-  align-items: center;
-  h1 {
-    text-align: center;
-    font-size: 25px;
-    font-weight: 600;
-    line-height: 1.3;
-    margin-bottom: 80px;
-  }
-`;
-const PlayerList = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  margin-bottom: -20px;
-  width: 100%;
-`;
-const PlayerDetail = styled.div`
-  width: 25%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 20px;
-  margin-bottom: 100px;
-  p {
-    font-size: 18px;
-    font-weight: 500;
-  }
 `;
-const PlayerImage = styled.div`
-  width: 20vw;
-  height: 20vw;
-  border-radius: 50%;
-  border: 1px solid ${COLORS.grey};
-  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-    url(${(props) => props.$bg});
-  background-size: contain;
-  background-repeat: no-repeat;
-  background-position: center;
-  cursor: pointer;
-
-  &:hover {
-    border: 1px solid ${COLORS.orange};
-    background-image: url(${(props) => props.$bg});
-  }
+const TitleText = styled.h1`
+  font-size: 35px;
+  font-weight: 700;
+  margin-bottom: 60px;
 `;
-const ProgressText = styled.p`
-  font-size: 20px;
-  font-weight: 800;
-  color: ${COLORS.grey};
-  margin-bottom: 50px;
+const VoteImage = styled.img`
+  width: 20%;
+  margin-bottom: 60px;
+  animation: ${fadeIn} 1s ease-in-out forwards;
+  filter: drop-shadow(0 0 4px ${COLORS.orange});
 `;
-const LinkWrap = styled.div`
-  display: flex;
-  gap: 50px;
+const SubText = styled.p`
+  font-size: 16px;
+  font-weight: 600;
+  margin-bottom: 40px;
 `;
 const CommonLink = styled(Link)`
-  border: 1px solid ${(props) => props.$border};
+  border: 1px solid ${COLORS.orange};
   border-radius: 5px;
-  padding: 15px 0;
-  width: 160px;
-  text-align: center;
+  padding: 15px 50px;
   text-decoration: none;
-  color: ${(props) => props.$fontColor};
+  color: ${COLORS.orange};
   font-size: 15px;
   font-weight: 300;
   background: linear-gradient(
     to right,
     ${COLORS.orange} 50%,
-    ${COLORS.black} 50%
+    rgba(0, 0, 0, 0) 50%
   );
   background-size: 200% 100%;
   background-position: right bottom;
   transition: all 0.5s ease-out;
 
   &:hover {
-    border: 1px solid ${COLORS.orange};
     color: ${COLORS.white};
     background-position: left bottom;
   }
 `;
 
-const VoteList = () => {
+const VoteEnd = () => {
   return (
     <VoteWrap>
-      <h1>
-        내가 바로 탱크!
-        <br />
-        당신의 투수를 투표해주세요.
-      </h1>
-      <PlayerList>
-        <PlayerDetail>
-          <PlayerImage $bg={Hwang} />
-          <p>황준서</p>
-        </PlayerDetail>
-        <PlayerDetail>
-          <PlayerImage $bg={Ryu} />
-          <p>류현진</p>
-        </PlayerDetail>
-        <PlayerDetail>
-          <PlayerImage $bg={Moon} />
-          <p>문동주</p>
-        </PlayerDetail>
-        <PlayerDetail>
-          <PlayerImage $bg={Lee} />
-          <p>이승관</p>
-        </PlayerDetail>
-        <PlayerDetail>
-          <PlayerImage $bg={Hwang} />
-          <p>황준서</p>
-        </PlayerDetail>
-        <PlayerDetail>
-          <PlayerImage $bg={Ryu} />
-          <p>류현진</p>
-        </PlayerDetail>
-        <PlayerDetail>
-          <PlayerImage $bg={Moon} />
-          <p>문동주</p>
-        </PlayerDetail>
-        <PlayerDetail>
-          <PlayerImage $bg={Lee} />
-          <p>이승관</p>
-        </PlayerDetail>
-      </PlayerList>
-      <ProgressText>1/10</ProgressText>
-      <LinkWrap>
-        <CommonLink $border={COLORS.grey} $fontColor={COLORS.white}>
-          BEFORE
-        </CommonLink>
-        <CommonLink
-          to="/vote/end"
-          $border={COLORS.orange}
-          $fontColor={COLORS.orange}
-        >
-          NEXT
-        </CommonLink>
-      </LinkWrap>
+      <TitleText>모든 투표가 끝났어요.</TitleText>
+      <VoteImage src={vote_end} alt="투표 종료" />
+      <SubText>수리들이 뽑은 선수들의 순위를 확인하러 갈까요?</SubText>
+      <CommonLink to="/vote/result">GO TO RANK</CommonLink>
     </VoteWrap>
   );
 };
 
-export default VoteList;
+export default VoteEnd;
--- hanwha/src/pages/vote/VoteEnd.js
+++ hanwha/src/pages/vote/VoteEnd.js
@@ -1,74 +1,98 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import styled, { keyframes } from 'styled-components';
 import { COLORS } from '../../constants/colors';
-import vote_end from '../../assets/vote_end.png';
+import { SIZES } from '../../constants/size';
+import { mediaMax } from '../../utils/media';
+import CustomBtn from '../../components/common/CustomBtn';
+import vote_end from '../../assets/vote/vote_end.png';
 
 const fadeIn = keyframes`
-    from {
-        opacity: 0;
-    }
-    to {
-        opacity: 1;
-    }
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
 `;
-const VoteWrap = styled.div`
-  width: 100%;
-  height: 100vh;
-  padding: 175.05px 0 100px 0;
-  background-color: ${COLORS.black};
+const neon = keyframes`
+  0% {
+    filter: drop-shadow(0 0 2px ${COLORS.orange});
+  }
+  50% {
+    filter: drop-shadow(0 0 10px ${COLORS.orange});
+  }
+  100% {
+    filter: drop-shadow(0 0 2px ${COLORS.orange});
+  }
+`;
+const MainWrap = styled.main`
   color: ${COLORS.white};
+  background-color: ${COLORS.black};
+`;
+const MainSection = styled.section`
+  animation: ${fadeIn} 1s ease-in-out forwards;
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding: 80px 0;
+  ${mediaMax.medium`
+    justify-content: center;
+    padding: 100px 0;
+    height: calc(100vh - 445px);
+  `};
+  ${mediaMax.small`
+    height: calc(100vh - 350px);
+  `};
 `;
 const TitleText = styled.h1`
-  font-size: 35px;
   font-weight: 700;
-  margin-bottom: 60px;
+  font-size: ${SIZES.ltxlarge};
+  padding-bottom: 50px;
+  ${mediaMax.medium`
+    padding-bottom: 60px;
+  `};
+  ${mediaMax.small`
+    font-size: ${SIZES.tbxlarge};
+    padding-bottom: 35px;
+  `};
 `;
-const VoteImage = styled.img`
+const VoteImg = styled.img`
+  animation: ${neon} 3s infinite ease-in-out;
   width: 20%;
-  margin-bottom: 60px;
-  animation: ${fadeIn} 1s ease-in-out forwards;
-  filter: drop-shadow(0 0 4px ${COLORS.orange});
+  margin-bottom: 50px;
+  ${mediaMax.medium`
+    width: 40%;
+  `};
+  ${mediaMax.small`
+    width: 50%;
+    margin-bottom: 45px;
+  `};
 `;
-const SubText = styled.p`
-  font-size: 16px;
+const SubText = styled.h2`
   font-weight: 600;
-  margin-bottom: 40px;
+  font-size: ${SIZES.ltmedium};
+  margin-bottom: 50px;
+  ${mediaMax.small`
+    font-size: ${SIZES.tbmedium};
+    margin-bottom: 35px;
+  `};
 `;
-const CommonLink = styled(Link)`
-  border: 1px solid ${COLORS.orange};
-  border-radius: 5px;
-  padding: 15px 50px;
-  text-decoration: none;
-  color: ${COLORS.orange};
-  font-size: 15px;
-  font-weight: 300;
-  background: linear-gradient(
-    to right,
-    ${COLORS.orange} 50%,
-    rgba(0, 0, 0, 0) 50%
-  );
-  background-size: 200% 100%;
-  background-position: right bottom;
-  transition: all 0.5s ease-out;
-
-  &:hover {
-    color: ${COLORS.white};
-    background-position: left bottom;
-  }
-`;
-
 const VoteEnd = () => {
   return (
-    <VoteWrap>
-      <TitleText>모든 투표가 끝났어요.</TitleText>
-      <VoteImage src={vote_end} alt="투표 종료" />
-      <SubText>수리들이 뽑은 선수들의 순위를 확인하러 갈까요?</SubText>
-      <CommonLink to="/vote/result">GO TO RANK</CommonLink>
-    </VoteWrap>
+    <MainWrap>
+      <MainSection>
+        <TitleText>모든 투표가 끝났어요.</TitleText>
+        <VoteImg src={vote_end} alt="투표 종료" />
+        <SubText>수리들이 뽑은 선수들의 순위를 확인하러 갈까요?</SubText>
+        <CustomBtn
+          to="/vote/result"
+          $border={COLORS.orange}
+          $fontColor={COLORS.orange}
+          $bgColor={COLORS.orange}
+          text="GO TO RANK"
+        />
+      </MainSection>
+    </MainWrap>
   );
 };
 
